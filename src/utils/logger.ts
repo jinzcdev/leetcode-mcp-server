5a9fae5e078255f4c42032cb103400d37acd2042
jinzcdev <jinzcdev@gmail.com>
2025-04-21 00:39:42 +0800

    feat: update logger configuration

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,6 @@
 import { pino } from "pino";
 
-const prodConfig = {
+const logger = pino({
     level: "info",
     formatters: {
         level: (label: string) => ({ level: label.toUpperCase() })
@@ -8,23 +8,6 @@ const prodConfig = {
     timestamp: () => `,"timestamp":"${new Date().toISOString()}"`,
     messageKey: "message",
     nestedKey: "payload"
-};
-
-const devConfig = {
-    level: "debug",
-    transport: {
-        target: "pino-pretty",
-        options: {
-            colorize: true,
-            translateTime: "SYS:yyyy-mm-dd HH:MM:ss.l",
-            ignore: "pid,hostname",
-            messageKey: "message"
-        }
-    }
-};
-
-const logger = pino(
-    process.env.NODE_ENV === "production" ? prodConfig : devConfig
-);
+});
 
 export default logger;
